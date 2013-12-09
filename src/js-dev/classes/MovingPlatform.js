49f98f50ae7371650fcee1a25248a4b9947460cb
Sven Lombaert <sven_93_06@hotmail.com>
2013-12-09 10:36:08 +0100

    updated with grunt

---
+++ src/js-dev/classes/MovingPlatform.js
@@ -0,0 +1,57 @@
+/*globals createjs:true*/
+var MovingPlatform = (function(){
+
+	function MovingPlatform(x, y, width, height, color, leftBound, rightBound, startOrientation, speed){
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.width = width;
+		this.height = height;
+		this.speed = speed;
+		this.orientation = startOrientation;
+		this.leftBound = leftBound;
+		this.rightBound = rightBound;
+		this.shape = new createjs.Shape();
+		this.shape.x = this.x;
+		this.shape.y = this.y;
+		var self = this;
+		self.draw();
+	}
+
+	MovingPlatform.prototype.draw = function() {
+		this.shape.graphics.c();
+		this.shape.graphics.f(this.color);
+		this.shape.graphics.dr(0, 0, this.width, this.height);
+		this.shape.graphics.ef();
+		this.move();
+	};
+
+	MovingPlatform.prototype.move = function() {
+		if(this.orientation === 'l'){
+			createjs.Tween.get(this).to({x:this.leftBound}, this.speed).call(this.changeOrientation);
+			createjs.Tween.get(this.shape).to({x:this.leftBound}, this.speed);
+			console.log(this.x);
+		}else{
+			createjs.Tween.get(this).to({x:this.rightBound}, this.speed).call(this.changeOrientation);
+			createjs.Tween.get(this.shape).to({x:this.rightBound}, this.speed);
+		}
+	};
+
+	MovingPlatform.prototype.changeOrientation = function() {
+		if(this.orientation === 'l') {
+			this.orientation = 'r';
+		} else {
+			this.orientation = 'l';
+		}
+		this.move();
+	};
+
+	MovingPlatform.prototype.setVisibility = function(visible) {
+		this.shape.visible = visible;
+	};
+
+
+
+	return MovingPlatform;
+
+})();
