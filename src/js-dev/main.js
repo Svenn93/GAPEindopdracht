f3619f5268c4f5b217a979286447a78355fe2aaa
D'Hondt Laurens <laurens.laurensdhondt@gmail.com>
2014-01-05 23:22:57 +0100

    Menu
    changed the menu

--- src/js-dev/main.js
+++ src/js-dev/main.js
@@ -2,17 +2,21 @@
 
 (function()
 {
-	var menuItems = ["PLAY","LEVELS","SCORES"];
+	var menuItems = ["PLAY","LEVELS","CONTROLS"];
 	var timer = 0;
 
 
 	function init()
 	{
 		menu();
+		$("#levels").hide();
+
+		console.log("hello");
 
 		setInterval(function(){
 			animation();
 		},1000);
+
 	}
 
 	function animation()
@@ -30,61 +34,70 @@
 		
 	}
 
+
 	function menu()
 	{
-		$("#previous").click(function(){
-
-			switch($(this).next().html())
+		$( "#previous").click(function(){
+			switch($("h1").html())
 			{
 				case menuItems[0]:
-					$(this).next().html(menuItems[2]);
+
+					$("h1").html(menuItems[2]);
+					$("h1").removeClass("hover");
+					$("#buttons").css("width","1200");
 				break;
 
 				case menuItems[1]:
-					$(this).next().html(menuItems[0]);
+					$("h1").html(menuItems[0]);
+					$("h1").addClass("hover");
+					$("#buttons").css("width","800");
+					$("#levels").fadeOut();
+					$("#menu").css("margin-top","20%");
 				break;
 
 				case menuItems[2]:
-					$(this).next().html(menuItems[1]);
+					$("h1").html(menuItems[1]);
+					$("#buttons").css("width","1000");
+					$("#levels").fadeIn();
+					$("#menu").css("margin-top","10%");
 				break;
 			}
 		});
 
 		$("#next").click(function(){
 
-			switch($(this).prev().html())
+			switch($("h1").html())
 			{
 				case menuItems[0]:
-					$(this).prev().html(menuItems[1]);
+					$("h1").html(menuItems[1]);
+					$("h1").removeClass("hover");
+					$("#buttons").css("width","1000");
+					$("#levels").fadeIn();
+					$("#menu").css("margin-top","10%");
 				break;
 
 				case menuItems[1]:
-					$(this).prev().html(menuItems[2]);
+					$("h1").html(menuItems[2]);
+					$("#buttons").css("width","1200");
+					$("#levels").fadeOut();
 				break;
 
 				case menuItems[2]:
-					$(this).prev().html(menuItems[0]);
+					$("h1").html(menuItems[0]);
+					$("#buttons").css("width","800");
+					$("h1").addClass("hover");
+					$("#menu").css("margin-top","20%");
 				break;
 			}
 		});
 
-		$("li").click(function(){
-			switch($(this).html())
+		$("h1").click(function(){
+			if($(this).html() === "PLAY")
 			{
-				case menuItems[0]:
 					console.log("play game");
 					$("#menu").remove();
 					$("canvas").css("display","block");
 					startGame();
-				break;
-
-				case menuItems[1]:
-					console.log("select level");
-				break;
-
-				case menuItems[2]:
-					console.log("leaderbord");
-				break;
 			}
 		});
 
